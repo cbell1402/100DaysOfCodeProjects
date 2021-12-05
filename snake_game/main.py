a916ac9be2bbef8c862f514438b06f43b00ad96a
Charles Bell <93935192+cbell1402@users.noreply.github.com>
2021-12-04 21:00:36 -0500

    Update main.py
    Adding high score ability

--- snake_game/main.py
+++ snake_game/main.py
@@ -26,23 +26,22 @@ while game_on:
     time.sleep(0.1)
     snake.move()
 
-    #Detect Food
+    # Detect Food
     if snake.head.distance(food) < 15:
         food.refresh()
         snake.extend()
         scoreboard.update_score()
 
-    #Detect Wall
+    # Detect Wall
     if snake.head.xcor() > 280 or snake.head.xcor() < -280 or snake.head.ycor() > 280 or snake.head.ycor() < -280:
-        scoreboard.game_over()
-        game_on = False
-        print("Game Over")
+        scoreboard.reset()
+        snake.reset()
 
-    #Detect Tail
+    # Detect Tail
     for parts in snake.body_parts[1:]:
         if snake.head.distance(parts) < 10:
-            game_on = False
-            scoreboard.game_over()
+            scoreboard.reset()
+            snake.reset()
 
 
 screen.exitonclick()
