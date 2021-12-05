f8c44f8121a038a7ac2ac2c39745ab4c733a9907
Charles Bell <93935192+cbell1402@users.noreply.github.com>
2021-12-04 20:59:49 -0500

    Update scoreboard.py
    Adding high score ability

--- snake_game/scoreboard.py
+++ snake_game/scoreboard.py
@@ -8,6 +8,8 @@ class ScoreBoard(Turtle):
     def __init__(self):
         super().__init__()
         self.current_score = 0
+        with open("High_Score.txt", mode="r") as file:
+            self.high_score = int(file.read())
         self.hideturtle()
         self.color("white")
         self.penup()
@@ -16,14 +18,17 @@ class ScoreBoard(Turtle):
 
     def update_scoreboard(self):
         self.clear()
-        self.write(f"Score: {self.current_score}", align=ALIGNMENT, font=FONT)
-        #self.current_score += 1
+        self.write(f"Score: {self.current_score} High Score: {self.high_score}", align=ALIGNMENT, font=FONT)
 
     def update_score(self):
         self.current_score += 1
         self.update_scoreboard()
 
-    def game_over(self):
-        self.goto(x=0, y=0)
-        self.write("GAME OVER.", align=ALIGNMENT, font=FONT)
+    def reset(self):
+        if self.current_score > self.high_score:
+            self.high_score = self.current_score
+            with open("High_Score.txt", mode="w") as file:
+                file.write(f"{self.high_score}")
+        self.current_score = 0
+        self.update_scoreboard()
 
