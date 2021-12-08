52f1e33570e105092afeafb3da69859935720285
Charles Bell <93935192+cbell1402@users.noreply.github.com>
2021-12-07 20:38:29 -0500

    Delete

--- main.py
+++
@@ -1,9 +0,0 @@
-import pandas
-
-NATO_df = pandas.read_csv("nato_phonetic_alphabet.csv")
-user_input = input("Please enter word to be converted: ").upper()
-
-NATO_dict = {row.letter: row.code for (index, row) in NATO_df.iterrows()}
-return_list = [NATO_dict[letter] for letter in list(user_input)]
-
-print(return_list)
